bd0fc87e0a923c7553fd923629c2faaa96caf488
Artyukov Nikita <nikita-amazar@mail.ru>
2024-10-09 20:19:12 +0800

    Соmmit 0.3.3.1: excerciseActivity фикс вылета

--- app/src/main/java/com/example/workauthelper/ExerciseActivity.java
+++ app/src/main/java/com/example/workauthelper/ExerciseActivity.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class ExerciseActivity extends AppCompatActivity {
 
     private ListView listView;
+
+    private ImageButton addButton; // Добавляем ImageButton для добавления
+    private ImageButton searchButton; // Добавляем ImageButton для поиска
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,24 +23,24 @@ public class ExerciseActivity extends AppCompatActivity {
         // Инициализация элементов интерфейса
         listView = findViewById(R.id.exercise_list_view);
         TextView title = findViewById(R.id.activity_title);
-        Button addButton = findViewById(R.id.add_button);
-        Button searchButton = findViewById(R.id.search_button);
+        addButton = findViewById(R.id.add_button); // Инициализируем ImageButton
+        searchButton = findViewById(R.id.search_button); // Инициализируем ImageButton
 
         // Установка названия активности
         title.setText("Упражнения");
 
-        // Обработка нажатий на кнопки
+        // Обработка нажатий на иконки
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Код для добавления нового упражнения
+                // Код для добавления новой категории
             }
         });
 
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Код для поиска упражнения
+                // Код для поиска категории
             }
         });
     }
